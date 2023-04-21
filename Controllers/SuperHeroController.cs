e0235fdbd75b84fa9fd48305626d36af5b093b54
dzdrum <dzdrum12@gmail.com>
2023-04-21 10:22:49 -0400

    first commit, .net core api with superhero controller

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class SuperHeroController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+        }
+    }
+}
+
