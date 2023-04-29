65bd3c338ab474f865d85e85fad93cc2c0f947fc
dzdrum <dzdzdrum12@gmail.com>
2023-04-29 13:52:03 +0000

    added interface and implementation for superhero service to help with unit testing and depedency injection

---
+++ Services/Implementations/SuperHeroService.cs
@@ -0,0 +1,25 @@
+﻿using SuperHeroAPI.Models;
+using SuperHeroAPI.Services.Interfaces;
+
+namespace SuperHeroAPI.Services.Implementations
+{
+    public class SuperHeroService : ISuperHeroService
+    {
+        public SuperHero Add(SuperHero newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SuperHero> GetAllSuperHeroes() => throw new NotImplementedException();
+
+        public SuperHero GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Interfaces/ISuperHeroService.cs
@@ -0,0 +1,12 @@
+﻿using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Services.Interfaces
+{
+    public interface ISuperHeroService
+    {
+        IEnumerable<SuperHero> GetAllSuperHeroes();
+        SuperHero Add(SuperHero newItem);
+        SuperHero GetById(int id);
+        void Remove(int id);
+    }
+}
